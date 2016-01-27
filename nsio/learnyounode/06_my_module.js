4cfe92cd63ccee3a372948e4d9b74a270d61c398
William Orr <orrwil@gmail.com>
2016-01-27 15:50:14 +0000

    Trying to get module working with callback

--- nsio/learnyounode/06_my_module.js
+++ nsio/learnyounode/06_my_module.js
@@ -2,19 +2,27 @@ function my_ls(path, fileExt, callback) {
     var fs = require('fs');
     
     function fileType(value) {
-        if (value.substr(value.length-(fileExt.length+1)) === fileExt){
-            return value;
+        if (value.substr(value.length-fileExt.length) === fileExt){
+            return blcavalue;
         }
     }
-    
-fs.readdir(path, function(err, list){
-        if (err) return callback(err);
-        var results = list.filter(fileType);
-        for (var i=0; i<results.length; i++){
-            return callback(results[i]);
+   
+    fs.readdir(path, function(err, list){
+        if (err) {
+            return callback(err);
+        }
+        else {
+            var results = list.filter(fileType);
+            // for (var i=0; i<results.length; i++){
+            return callback(results);
         }
     });
-    return callback();
-}
+};
 
-module.exports = my_ls;
+my_ls("./", "js", function(err,list) {
+    if (err) return err;
+    for (var i=0; i<list.length; i++){
+        console.log(list[i]);
+    }
+});
+//module.exports = my_ls;
